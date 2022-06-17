70b8f4970930bb1637c88fd96421124daf5a9e5a
Onbashi-Meriem <meryemonbasi5@gmail.com>
2022-06-17 12:54:54 +0200

    #inputRef added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,35 @@ export class AppComponent {
 
   unDoneList:UnDoneModel[]=[];
   doneList:DoneModel[]=[]
-  unDoneInput:string=''
+  // unDoneInput:string=''
 
-  addUnDoneItem(){
+  addUnDoneItem(inputRef:HTMLInputElement){
+    // console.log(inputRef)
     
     let unDoneItem=new UnDoneModel();
     unDoneItem.date=Date();
-    unDoneItem.title=this.unDoneInput
+    unDoneItem.title=inputRef.value
 
     this.unDoneList.push(unDoneItem)
+    inputRef.value=""
   }
+
+  addDoneList(unDone:UnDoneModel){
+    const index=this.unDoneList.indexOf(unDone)
+    this.unDoneList.splice(index,1)
+
+    this.doneList.push(unDone)
+  }
+
+  deleteTask(unDone:UnDoneModel){
+    const index=this.unDoneList.indexOf(unDone)
+    this.unDoneList.splice(index,1)
+  }
+returnUnDoneList(done:DoneModel){
+  const index=this.unDoneList.indexOf(done)
+  this.doneList.splice(index,1)
+
+  this.unDoneList.push(done)
+}
+
 }
